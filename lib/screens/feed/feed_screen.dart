f5b31a7aa40f8c598a04d18df0d76e9b5256c591
Veli Bacık <velibacik60@gmail.com>
2019-05-27 00:50:32 +0300

    sliver app bar left to center add

--- lib/screens/feed/feed_screen.dart
+++ lib/screens/feed/feed_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:oneframe/shared/views/window.dart';
 import 'package:oneframe/widgets/card/list_card.dart';
 
 class FeedPage extends StatefulWidget {
@@ -10,10 +12,13 @@ class FeedPage extends StatefulWidget {
 
 class _FeedPageState extends State<FeedPage> {
   List<String> _datas;
+  ScrollController _controller;
+  double screenWidth;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
     //Example
     _datas = new List<String>();
     for (var i = 0; i < 15; i++) {
@@ -21,20 +26,41 @@ class _FeedPageState extends State<FeedPage> {
     }
   }
 
+  final String _title = "Settings";
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
-          title: Text('SliverAppBar'),
+          pinned: true,
           backgroundColor: Colors.green,
           expandedHeight: 200.0,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
+          flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              print('constraints=' + constraints.toString());
+              return FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: AnimatedOpacity(
+                      duration: Duration(milliseconds: 300),
+                      //opacity: top == 80.0 ? 1.0 : 0.0,
+                      opacity: 1.0,
+                      child: constraints.maxHeight ==
+                              WindowHelper.BAR_HEIGHT(context)
+                          ? Text(this._title)
+                          : Container(
+                              alignment: Alignment.bottomLeft,
+                              padding: EdgeInsets.only(left: WindowHelper.SLIVER_LEFT_PADDING),
+                              child: Text(this._title))),
+                  background: Image.network(
+                    "https://images.unsplash.com/photo-1542601098-3adb3baeb1ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5bb9a9747954cdd6eabe54e3688a407e&auto=format&fit=crop&w=500&q=60",
+                    fit: BoxFit.cover,
+                  ));
+            },
           ),
         ),
         SliverFixedExtentList(
-          itemExtent: 150.0,
+          itemExtent: 200.0,
           delegate: SliverChildListDelegate(
             [
               Container(color: Colors.red),
@@ -49,4 +75,20 @@ class _FeedPageState extends State<FeedPage> {
       ],
     );
   }
+}
+
+class Delegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+          BuildContext context, double shrinkOffset, bool overlapsContent) =>
+      Container(color: Colors.yellow);
+
+  @override
+  double get maxExtent => 60;
+
+  @override
+  double get minExtent => 30;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
 }
---
+++ lib/shared/views/window.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WindowHelper {
+  static AppBar _appBar = AppBar(
+    title: Text('App bar height'),
+  );
+  static double BAR_HEIGHT(BuildContext context) {
+    final _appBarHeight =
+        MediaQuery.of(context).padding.top + _appBar.preferredSize.height;
+    return _appBarHeight;
+  }
+
+  static double SLIVER_LEFT_PADDING = 76;
+}
